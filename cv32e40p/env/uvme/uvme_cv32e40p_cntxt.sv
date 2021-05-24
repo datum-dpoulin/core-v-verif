a771990591227a34e51a8b50a82deea414fbd283
David Poulin <dpoulin@datumtc.ca>
2021-05-24 14:46:56 -0400

    Added virtual sequences to cv32e40p UVM environment to implement instruction and data memory (as well as virtual peripherals.
    Signed-off-by: David Poulin <dpoulin@datumtc.ca>

--- cv32e40p/env/uvme/uvme_cv32e40p_cntxt.sv
+++ cv32e40p/env/uvme/uvme_cv32e40p_cntxt.sv
@@ -32,15 +32,19 @@ class uvme_cv32e40p_cntxt_c extends uvm_object;
    virtual uvmt_cv32e40p_debug_cov_assert_if debug_cov_vif;
 
    // Agent context handles
-   uvma_clknrst_cntxt_c    clknrst_cntxt;
-   uvma_interrupt_cntxt_c  interrupt_cntxt;
-   uvma_debug_cntxt_c      debug_cntxt;
-   uvma_obi_cntxt_c        obi_instr_cntxt;
-   uvma_obi_cntxt_c        obi_data_cntxt;
+   uvma_clknrst_cntxt_c     clknrst_cntxt;
+   uvma_interrupt_cntxt_c   interrupt_cntxt;
+   uvma_debug_cntxt_c       debug_cntxt;
+   uvma_obi_cntxt_c         obi_instr_cntxt;
+   uvma_obi_cntxt_c         obi_data_cntxt;
    
    // TODO Add scoreboard context handles
    //      Ex: uvme_cv32e40p_sb_cntxt_c  sb_egress_cntxt;
    //          uvme_cv32e40p_sb_cntxt_c  sb_ingress_cntxt;
+   
+   // Memory modelling
+   bit [31:0]  mem[2**8];
+   bit         instr_mem_delay_enabled = 0;
    
    // Events
    uvm_event  sample_cfg_e;
@@ -48,11 +52,11 @@ class uvme_cv32e40p_cntxt_c extends uvm_object;
    
    
    `uvm_object_utils_begin(uvme_cv32e40p_cntxt_c)
-      `uvm_field_object(clknrst_cntxt,   UVM_DEFAULT)
-      `uvm_field_object(interrupt_cntxt, UVM_DEFAULT)
-      `uvm_field_object(debug_cntxt  ,   UVM_DEFAULT)
-      `uvm_field_object(obi_instr_cntxt, UVM_DEFAULT)
-      `uvm_field_object(obi_data_cntxt,  UVM_DEFAULT)
+      `uvm_field_object(clknrst_cntxt         , UVM_DEFAULT)
+      `uvm_field_object(interrupt_cntxt       , UVM_DEFAULT)
+      `uvm_field_object(debug_cntxt           , UVM_DEFAULT)
+      `uvm_field_object(obi_instr_cntxt       , UVM_DEFAULT)
+      `uvm_field_object(obi_data_cntxt        , UVM_DEFAULT)
       
       // TODO Add scoreboard context field macros
       //      Ex: `uvm_field_object(sb_egress_cntxt , UVM_DEFAULT)
